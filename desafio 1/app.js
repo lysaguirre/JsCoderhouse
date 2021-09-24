e7cc612599da0a0a9d8b3b83161526b51e682387
lysaguirre <lysaguirre@icloud.com>
2021-09-24 16:49:51 -0300

    tareas subidas

---
+++ desafio 1/app.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+let numero = 10
+let numero2 = Number(prompt('ingrese un numero'))
+
+console.log(numero + numero2)
+
+
---
+++ desafio 2/app.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+let edad = Number(prompt('Ingresa una edad'))
+
+
+if (edad >= 21) {
+    alert("Sos mayor de edad")
+} 
+
+else if(edad <21){
+    alert("Sos menor de edad")
+}
+
+
